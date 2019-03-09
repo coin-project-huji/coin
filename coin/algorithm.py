0a97e3b42f4751a6d833dd5fca071f84b8b55f27
livneros <livneros@gmail.com>
2019-03-09 23:06:29 +0200

    adding no results message - entirely

--- coin/algorithm.py
+++ coin/algorithm.py
@@ -81,9 +81,9 @@ def run(string_content):
 
 
 def get_results_map(content, user_input, res):
-    result = "The top results for your search: " + user_input + ", are:\n"
     if not res:
         return "We are sorry, but no results found"
+    result = "The top results for your search: " + user_input + ", are:\n"
     for row in res:
         try:
             if row.a_node != content:
